51dd16a2f0b92e31dbcf16bc600bebb968b05725
vladimir-var <vovavarenik0@gmail.com>
2024-05-04 16:13:49 +0300

    pulling from back is done

---
+++ columnPattern.js
@@ -0,0 +1,52 @@
+var columnPattern = `
+    <div class="main-card-container rounded-2" id="PLACEHOLDERid">
+    <div class="header-card-container">
+        <p class="main-card-p">PLACEHOLDERtitle</p>
+        <p class="main-card-men">•••</p>
+    </div>
+    <div class="helping-container"id="PLACEHOLDERid">
+        <a href="#create" rel="modal:open">
+            <div class="card-plus-but rounded-2">
+                <div class="animation-wrapper">
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                    <span></span>
+                </div>
+                <img src="assets/plus.png" class="card-m-img plus-img">
+            </div>
+        </a>
+    </div>
+    </div>
+`;
+
+function createColumnWithData(title,id) {
+    var stringRes = columnPattern.replace("PLACEHOLDERtitle", title);
+    stringRes= stringRes.replaceAll("PLACEHOLDERid", id);
+    return stringRes;
+}
+
+
+var cardPattern = `
+ 
+            <div class="main-card rounded-2">
+                <div class="card-header-man">
+                    <p class="card-tag card-sm-text">Backend</p>
+                </div>
+                <div class="card-main">
+                    <p class="card-main-text card-text">PLACEHOLDERtitle</p>
+                </div>
+                <div class="card-footer-man">
+                    <div class="d-inline-flex">
+                        <img src="assets/free-icon-clock-2088617.png" class="card-sm-img clock-img">
+                        <p class="card-text card-sm-text text-footer sm-mar-l">PLACEHOLDERdeadline</p>
+                    </div>
+                </div>
+            </div>
+
+`;
+function createCardWithData(title, label, text, deadline) {
+    var stringRes = cardPattern.replace("PLACEHOLDERtitle", title);
+    stringRes = stringRes.replace("PLACEHOLDERdeadline", deadline);
+    return stringRes;
+}
--- index.js
+++ index.js
@@ -1,9 +1,10 @@
-var endpoint = "https://localhost:8080/";
+var endpoint = "https://localhost:7193/";
 var cardsEndpoint = "api/cards/";
 var statusEndpoint = "api/statuses/";
 
 
-$(document).ready(function() {
+
+$(document).ready(function () {
     var colums;
 
     $.ajax({
@@ -11,13 +12,106 @@ $(document).ready(function() {
         url: endpoint + statusEndpoint,
         dataType: "json",
         async: false,
-        success: function(response){
-            colums = JSON.parse(response.response);
+        success: function (response) {
+            colums = response;
         },
-        error: function(jqXHR, textStatus, errorThrown) {
+        error: function (jqXHR, textStatus, errorThrown) {
             alert(textStatus + ": " + errorThrown);
         }
     });
+
+    $.ajax({
+        type: "GET",
+        url: `${endpoint}${statusEndpoint}`,
+        dataType: "json",
+        headers: {
+            'Accept': 'application/json',
+            'Content-Type': 'application/json'
+        },
+        success: function (response) {
+            console.log(response);
+            Object.keys(response).forEach(item => {
+                console.log(response[item]);
+                $("article.main-cards-container").append(createColumnWithData(response[item].name, response[item].id));
+            })
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            // В случае ошибки вы можете вывести сообщение об ошибке
+            console.error(`Ошибка при получении данных о карточках: ${textStatus} - ${errorThrown}`);
+        }
+    });
+    $.ajax({
+        type: "GET",
+        url: `${endpoint}${cardsEndpoint}`,
+        dataType: "json",
+        success: function (response) {
+            console.log(response);
+            response.forEach(item => {
+                console.log(item.idStatus);
+                $("#"+item.idStatus+".helping-container").prepend(createCardWithData(item.title, item.label, item.title, item.deadline));
+            });
+            
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+            console.error(`Error: ${textStatus} - ${errorThrown}`);
+        }
+    });
+
+
+
+    // function createColumnsWithData(cardData) {
+    //     var columns = {};
+
+    //     cardData.forEach(function (card) {
+    //         // Если столбец для данной категории еще не создан, создаем его
+    //         if (!columns[card.category]) {
+    //             columns[card.category] = $(columnPattern);
+    //             columns[card.category].find('.main-card-p').text(card.category);
+    //         }
+
+    //         var columnElement = columns[card.category];
+
+    //         var cardElement = $("<div>").addClass("main-card rounded-2");
+    //         cardElement.append($("<div>").addClass("card-header-man").append($("<p>").addClass("card-tag card-sm-text").text(card.label)));
+    //         cardElement.append($("<div>").addClass("card-main").append($("<p>").addClass("card-main-text card-text").text(card.title)));
+    //         cardElement.append($("<div>").addClass("card-footer-man").append($("<div>").addClass("d-inline-flex").append($("<img>").addClass("card-sm-img clock-img").attr("src", "assets/free-icon-clock-2088617.png"), $("<p>").addClass("card-text card-sm-text text-footer sm-mar-l").text(card.startDate))));
+
+    //         columnElement.find(".main-card-container").append(cardElement);
+    //     });
+
+    //     // Возвращаем массив столбцов
+    //     return Object.values(columns);
+    // }
+
+    // // Запрос AJAX для получения данных о карточках
+    // $.ajax({
+    //     type: "GET",
+    //     url: `${endpoint}${cardsEndpoint}`,
+    //     dataType: "json",
+    //     headers: {
+    //         'Accept': 'application/json',
+    //         'Content-Type': 'application/json'
+    //     },
+    //     success: function (response) {
+    //         // После успешного получения данных о карточках
+    //         console.log(response);
+    //         // Создаем столбцы с данными о карточках и добавляем на страницу
+    //         var columns = createColumnsWithData(response);
+    //         columns.forEach(function(column) {
+    //             $("article.main-cards-container").append(column);
+    //         });
+    //     },
+    //     error: function (jqXHR, textStatus, errorThrown) {
+    //         // В случае ошибки вы можете вывести сообщение об ошибке
+    //         console.error(`Ошибка при получении данных о карточках: ${textStatus} - ${errorThrown}`);
+    //     }
+    // });
+
+
+
+
+
+
     // $.ajax({
     //     type: "GET",
     //     url: endpoint + cardsEndpoint,
@@ -47,7 +141,7 @@ $(document).ready(function() {
 
     // $("#buttonColumnCreate").click(function() {
     //     var title = $("#titleColumnCreate").val();
-        
+
     //     $.post(endpoint + statusEndpoint,
     //     {
     //         Name: title,
@@ -59,13 +153,13 @@ $(document).ready(function() {
 
     // Инициализация Dragula для всех контейнеров с классом helping-container
     var drake = dragula($('.helping-container').toArray(), {
-        invalid: function(el, handle) {
+        invalid: function (el, handle) {
             return el.classList.contains('card-plus-but');
         }
     });
 
     // Обработчик события, который вызывается при перемещении объекта
-    drake.on('drop', function(el, target, source, sibling) {
+    drake.on('drop', function (el, target, source, sibling) {
         // el - перемещаемый элемент (div с классом main-card)
         // target - элемент, в который перемещен объект (div с классом helping-container)
         // source - исходный элемент, из которого перемещен объект (div с классом helping-container)
