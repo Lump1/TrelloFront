9244d892f1b7b42c15f6a75b335d6fabbf318464
Lump1 <dimond_hurt@icloud.com>
2024-06-04 00:15:38 +0200

    Board adding opperations

--- home_page.js
+++ home_page.js
@@ -2,9 +2,40 @@
 var endpoint = "https://localhost:7193/";
 var boardsEndpoint = "api/boards/";
 var usersBoardEndpoint = "api/users/boards/";
-var usersEndpoint = "api/users/"
+var usersEndpoint = "api/users/";
+var teamEndpoint = "api/teams/";
 var teamuserEndpoint = "api/team-user/";
 
+
+
+function getRandomArbitrary(min, max) {
+  return Math.random() * (max - min) + min;
+}
+
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max);
+}
+
+function getRandomIntInc(min, max) {
+  min = Math.ceil(min);
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1)) + min;
+}
+
+function generateName() {
+  var subWords = ["Super", "Hyper", "Cool", "Justice", "Strong", "Loonar", "Sunny"]
+  var words = ["wo", "de", "mu", "re", "ba", "fo", "hu", "ki", "we", "by", "pu", "le", "lo", "fa", "da"];
+  var tempText;
+  tempText = "";
+
+  for(let i = 0; i < getRandomIntInc(3, 4); i++) {
+    console.log(i);
+    tempText += words[getRandomInt(words.length)];
+  }
+
+  console.log(getRandomInt(subWords.length));
+  return subWords[getRandomInt  (subWords.length)] + " " + tempText;
+}
 
 function boardCardRender(board, target="AllBoards"){
   getQuerryTemplate("Board", { name: board.name, id: board.id}).then(
@@ -66,28 +97,75 @@ function clickReload() {
 }
 
 function createTeamAjax() {
-  var usersArray = $(".team-list-item-content").toArray().map(item => {
-    return item.id;
-  });
-  usersArray.push(JSON.parse(Cookies.get("userGUID")).id);
-
   return new Promise((resolve, reject) => {
     $.ajax({
       type: "POST",
-      url: `${endpoint}${teamendpoint}`,
+      url: `${endpoint}${teamEndpoint}`,
+      dataType: "json",
       headers: {
         Accept: "application/json",
         "Content-Type": "application/json",
       },
-      data: {name: boardname, teamUsers: usersArray},
+      data: JSON.stringify({name: "teamname"}),
 
       success: function(data) {
-        var teamid = data.id
+        var teamid = data.id;
         resolve(teamid);
-      }
+      },
+      error: function (jqXHR, textStatus, errorThrown) {
+        console.log("AJAX error:", textStatus, errorThrown);
+        console.log("Response:", jqXHR.responseText);
+      },
     })
   });
 }
+
+function pushUsersAjax(teamid) {
+  var usersArray = $(".team-list-item-content").toArray().map(item => {
+    return item.id;
+  });
+
+  // usersArray.push(Cookies.get("userGUID").id);
+
+  usersArray.forEach(userid => {
+    console.log(userid)
+    $.ajax({
+      type: "POST",
+      url: `${endpoint}${teamuserEndpoint}`,
+        headers: {
+          Accept: "application/json",
+          "Content-Type": "application/json",
+        },
+        data: JSON.stringify({idTeam: teamid, idUser: userid}),
+
+        success: function(data) {
+          console.log(data);
+        },
+        error: function (jqXHR, textStatus, errorThrown) {
+          console.log("AJAX error:", textStatus, errorThrown);
+          console.log("Response:", jqXHR.responseText);
+        },
+    });
+  });
+}
+
+// function addUserToTeam(username, teamid) {
+//   new Promise((resolve, reject) => {
+//     $.ajax({
+//       type: "POST",
+//       url: `${endpoint}${teamEndpoint}`,
+//         headers: {
+//           Accept: "application/json",
+//           "Content-Type": "application/json",
+//         },
+//         data: {idTeam: teamid, idUser: },
+
+//         success: function(data) {
+//           var teamid = data.id;
+//         }
+//     });
+//   });
+// }
 
 function addUserAjax(teamid, userid) {
   $.ajax({
@@ -108,15 +186,20 @@ function addUserAjax(teamid, userid) {
 function createBoard(teamid) {
   $.ajax({
     type: "POST",
-    url: `${endpoint}${boardendpoint}`,
+    url: `${endpoint}${boardsEndpoint}`,
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json",
     },
-    data: {name: boardname, idTeam: teamid},
+    data: JSON.stringify({name: generateName(), idTeam: teamid}),
     success: function(board) {
+      console.log(board);
       boardCardRender(board);
-    }
+    },
+    error: function (jqXHR, textStatus, errorThrown) {
+      console.log("AJAX error:", textStatus, errorThrown);
+      console.log("Response:", jqXHR.responseText);
+    },
   })
 }
 
@@ -133,6 +216,7 @@ function getUser(username) {
 
       success: function(data) {
         var user = data;
+        console.log(data);
         resolve(user);
       }
     })
@@ -178,14 +262,19 @@ $(document).ready(function(){
 
   $("#search_user_button").on("mouseup", function() {
     getUser($("#search_user_input").val()).then((user) => {
-      getQuerryTemplate("Teamusercard", {id: user.id, username: user.username}).then((resultHTML) =>{
-        $(".team-list-item").append(resultHTML);
-      })   
+      Object.keys(user).forEach(key => {
+        var tempUser = user[key];
+
+        getQuerryTemplate("Teamusercard", {id: tempUser.id, username: tempUser.username}).then((resultHTML) =>{
+          $(".team-list-item").append(resultHTML);
+        })   
+      })
     })
   });
 
   $("#boardCreationWithoutTemplate").on("mouseup", function() {
     createTeamAjax().then((teamid) => {
+      pushUsersAjax(teamid);
       createBoard(teamid);
     })
   })
